8618294d1fc2b9f34d3ff71cf76bbc5e625b6705
Mille <valentin.mille@epitech.eu>
2023-02-14 07:21:47 +0100

    feat(project): init base project

---
+++ MVVMTechnicalTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVMTechnicalTest
+//
+//  Created by Valentin Mille on 2/14/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
